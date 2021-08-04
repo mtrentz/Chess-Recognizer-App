85c442509b95168fae7b7f6c9dfde9dd53cb0ca5
mateus trentz <mateus.trentz@gmail.com>
2021-08-04 20:20:52 -0300

    Changed label of board img form field

--- recognizer/forms.py
+++ recognizer/forms.py
@@ -5,3 +5,7 @@ class BoardUploadForm(forms.ModelForm):
     class Meta:
         model = Board
         fields = ['board_img']
+    
+    def __init__(self, *args, **kwargs):
+        super(BoardUploadForm, self).__init__(*args, **kwargs)
+        self.fields['board_img'].label = 'Board Image'
