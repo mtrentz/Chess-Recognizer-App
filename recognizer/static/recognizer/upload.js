c9af7606f4aabbf016794b30f6d20c653c5de57c
mateus trentz <mateus.trentz@gmail.com>
2021-08-04 20:58:13 -0300

    added some margins to upload page.

--- recognizer/static/recognizer/upload.js
+++ recognizer/static/recognizer/upload.js
@@ -1,17 +1,21 @@
 const radioWhite = document.querySelector("[data-white]")
 const radioBlack = document.querySelector("[data-black]")
-var fen_text = document.querySelector("[data-fen]")
+var fenText = document.querySelector("[data-fen]")
+var lichessBtn = document.querySelector("#lichess-btn")
 
 const fenData = JSON.parse(document.getElementById("fen_data").textContent);
+const lichessUrls = JSON.parse(document.getElementById("lichess_urls").textContent);
 
 if (radioWhite != null){
     radioWhite.onclick = function(){
-        fen_text.innerHTML = fenData.fen_white
+        fenText.value = fenData.fen_white
+        lichessBtn.href = lichessUrls.play_white_url
     }
 }
 
 if (radioBlack != null){
     radioBlack.onclick = function(){
-        fen_text.innerHTML = fenData.fen_black
+        fenText.value = fenData.fen_black
+        lichessBtn.href = lichessUrls.play_black_url
     }
 }
