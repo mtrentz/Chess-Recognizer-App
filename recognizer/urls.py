2153479e1183d30c86f55c7b1e87a866f9738180
mateus trentz <mateus.trentz@gmail.com>
2021-08-04 21:10:15 -0300

    removed home and about from base

--- recognizer/urls.py
+++ recognizer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='recognizer-home'),
-    path('about/', views.about, name='recognizer-about'),
-    path('upload/', views.upload, name='recognizer-upload'),
+    path('', views.upload, name='recognizer-home'),
+    # path('about/', views.about, name='recognizer-about'),
+    # path('upload/', views.upload, name='recognizer-upload'),
 ]
